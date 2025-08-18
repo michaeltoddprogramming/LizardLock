bcac815bce30efca2c7c8214599dbeb199fd33c6
Michael Todd <michaeltodd981@gmail.com>
2025-08-18 20:43:08 +0200

    basic styles for clarity reasons - I hate plain text on web - too much doom scrolling

--- LizardAccounts/models.py
+++ LizardAccounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lizards(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('user', 'User'),
+        ('guest', 'Guest'),
+    ]
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='guest')
+    is_approved = models.BooleanField(default=False)
+    mfa_secret = models.CharField(max_length=32, blank=True, null=True)
--- LizardLock/settings.py
+++ LizardLock/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'LizardAccounts'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'filefortress.urls'
+ROOT_URLCONF = 'LizardLock.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'filefortress.wsgi.application'
+WSGI_APPLICATION = 'LizardLock.wsgi.application'
 
 
 # Database
